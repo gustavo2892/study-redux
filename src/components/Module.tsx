8058db653bdd34c5b47267a41fdce3f6c26cba99
gustavo2892 <gustavo2892@gmail.com>
2024-01-21 18:30:58 -0300

    Add play action

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,8 +1,10 @@
 import * as Collapsible from '@radix-ui/react-collapsible';
 import { ChevronDown } from 'lucide-react';
+import { useDispatch } from 'react-redux';
 
 import { Lesson } from './Lesson';
 import { useAppSelector } from "../store";
+import { play } from '../store/slices/player';
 
 interface ModuleProps {
   moduleIndex: number;
@@ -11,9 +13,11 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+  const dispatch = useDispatch();
+  
   const lessons = useAppSelector(state => {
     return state.player.course.modules[moduleIndex].lessons
-  })
+  });
 
   return (
     <Collapsible.Root className="group">
@@ -31,12 +35,13 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
       </Collapsible.Trigger>
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson) => {
+          {lessons.map((lesson, lessonIndex) => {
             return (
               <Lesson
                 key={lesson.id}
                 title={lesson.title}
                 duration={lesson.duration}
+                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
               />
             )
           })}
